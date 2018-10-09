948a761b59eb5d53144b648ac3fd86a942520c30
kons16 <ks1216r@gmail.com>
2018-10-09 23:46:23 +0900

    学生が所属している大学の授業のみを表示

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,4 +1,5 @@
 <?php
+// ログインしたときにトップページに表示するビューを表示
 
 namespace App\Http\Controllers;
 
@@ -11,14 +12,21 @@ class PagesController extends Controller
 {
 	// トップページにアクセスしたとき
     public function show() {
-    	$user = Auth::user();
-		$userid = Auth::id();
+    	$user = Auth::user();	// ログインしているユーザを取得
 
 		// 教員が持っている授業 と 学生が出席できる授業 の表示
 		if($user->student_id == 'teacher'){
+			// 教員側
 			$lectures = $user->lectures;
 		}else{
-			$lectures = Lecture::all();
+			// 学生側 表示されるのは同じ大学のみ
+			$all = Lecture::all();
+			$lectures = [];
+			foreach($all as $a){
+				if($a->univ == $user->univ){
+					array_push($lectures, $a);
+				}
+			}
 		}
 
 		return view('lectures', [
