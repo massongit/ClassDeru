ce11265de58a6c95adaa263ac1ed8d660225cd5b
kons16 <ks1216r@gmail.com>
2018-12-03 20:57:16 +0900

    授業を追加のレイアウトを修正

--- resources/views/lectures.blade.php
+++ resources/views/lectures.blade.php
@@ -2,82 +2,67 @@
 
 <!-- ログインしている教員側に表示する授業登録画面 -->
 @section('content')
+<center>
 	<div class="container">
 		<div class="col-sm-offset-2 col-sm-8">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<B>新規授業</B>
-				</div>
 
-				<div class="panel-body">
-					<!-- エラーの確認 -->
-					@include('common.errors')
+			<div class="box_lec">
+        <span class="box-title">授業を追加</span>
+        <p>
+          <form action="/user/lecture" method="POST" class="form-horizontal">
+              {{ csrf_field() }}
 
-					<!-- 新規授業登録フォーム -->
-					<form action="/user/lecture" method="POST" class="form-horizontal">
-						{{ csrf_field() }}
+            <div class="form-group">
+              <div class="form-group col-md-4">
+                <label for="task-name">授業名</label>
+                <input type="text" name="title" placeholder="(例) 数学解析" id="title" class="form-control" value="{{ old('title') }}">
+              </div>
+            </div>
 
-						<!-- 追加する授業の情報 -->
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">授業名</label>
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="task-name">開講する大学</label>
 
-							<div class="col-sm-6">
-								<input type="text" name="title" placeholder="(例) 数学解析" id="title" class="form-control" value="{{ old('title') }}">
-							</div>
+                <input type="text" name="univ" placeholder="(例) 茨城" id="univ" class="form-control" value="{{ old('univ') }}">
+              </div>
+              
+              <div class="form-group col-md-4">
+                <label for="task-name">学部</label>
 
+                  <input type="text" name="gra" id="gra" class="form-control" placeholder="(例) 工" value="{{ old('gra') }}">
+              </div>
 
-							<label for="task-name" class="col-sm-3 control-label">開講する大学</label>
+              <div class="form-group col-md-4">
+                <label for="task-name" class="control-label">学科</label>
 
-							<div class="col-sm-6">
-								<input type="text" name="univ" placeholder="(例) 茨城" id="univ" class="form-control" value="{{ old('univ') }}">
-							</div>
-
-							<label for="task-name" class="col-sm-3 control-label">開講する学部</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="gra" id="gra" class="form-control" placeholder="(例) 工"value="{{ old('gra') }}">
-							</div>
-
-							<label for="task-name" class="col-sm-3 control-label">開講する学科</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="dep" id="dep" class="form-control" placeholder="(例) 情報工" value="{{ old('dep') }}">
-							</div>
+                <input type="text" name="gra" id="gra" class="form-control" placeholder="(例) 情報工" value="{{ old('dep') }}">
+              </div>
+            </div>
 
 
-							<label for="task-name" class="col-sm-3 control-label">全受講者数</label>
+            <div class="form-group">
+              <div class="col-sm-4">
+                <label for="task-name" class="control-label">全受講者数</label>
+                <input type="text" name="gra" id="gra" class="form-control" placeholder="(例) 100" value="{{ old('number') }}">
+              </div>
+            </div>
 
-							<div class="col-sm-6">
-								<input type="text" name="number" placeholder="(例) 100" id="number" class="form-control" value="{{ old('number') }}">
-							</div>
+            <div class="form-group">
+              <div class="col-sm-4">
+                <label for="task-name" class="control-label">教室・連絡</label>
+                <input type="text" name="gra" id="gra" class="form-control" value="{{ old('date') }}">
+              </div>
+            </div>
 
-
-							<label for="task-name" class="col-sm-3 control-label">教室・連絡</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="date" id="date" class="form-control" value="{{ old('date') }}">
-							</div>
-
-
-							<label for="task-name" class="col-sm-3 control-label">パスワード(未記入可)</label>
-
-							<div class="col-sm-6">
-								<input type="text" name="lecpass" id="lecpass" class="form-control" autocomplete="off" value="{{ old('lecpass') }}">
-							</div>
-						</div>
-
-
-						<!-- 授業を追加 -->
-						<div class="form-group">
-							<div class="col-sm-offset-3 col-sm-6">
-								<button type="submit" class="btn btn-success">
-									授業を追加する
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+            <div class="form-group">
+               <div class="col-sm-4">
+                <label for="task-name" class="control-label">パスワード(未入力可)</label>
+                <input type="text" name="gra" id="gra" class="form-control" value="{{ old('lecpass') }}">
+              </div>
+            </div>
+          </form>
+        </p>
+    </div>
 
 			<!-- 授業一覧 -->
 			@if (count($lectures) > 0)
@@ -204,7 +189,7 @@
 				</table>
 			</div>
 		</div>
-
+  </center>
 	@else
 
 		<div class="container mt-2">
